74cd2581d1eae55bd34726db798d87cc5405d655
Chris Zavala <zavastopher@gmail.com>
2024-11-11 20:58:15 -0500

    Driver running, attempting to connect to database

--- api/name-gen/main.go
+++ api/name-gen/main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 )
 
 func main() {
 	fmt.Println("Name Gen")
-	connstr := "postgres://oracle:supersecurepassword@localhost:3210/randomtables?sslmode=verify-full"
+	connstr := "postgres://oracle:supersecurepassword@localhost:3210/random-tables?sslmode=verify-full"
 	db, err := sql.Open("postgres", connstr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rows, err := db.Query("SELECT name FROM names WHERE Category = Male")
-	fmt.Println(rows)
-
+	rows, err := db.Query("SELECT name FROM names WHERE category = Male")
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%s", name)
+	}
 }
