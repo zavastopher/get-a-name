5875daaa5691a63d82297633ecf636ed818512a1
Chris Zavala <zavastopher@gmail.com>
2024-11-13 20:18:27 -0500

    name generator is now correctly connecting to the correct database

--- api/name-gen/main.go
+++ api/name-gen/main.go
@@ -8,19 +8,31 @@ import (
 )
 
 func main() {
-	fmt.Println("Name Gen")
-	connstr := "postgres://oracle:supersecurepassword@localhost:3210/random-tables?sslmode=verify-full"
+	fmt.Println("connecting to random-tables db")
+	connstr := "postgres://oracle:supersecurepassword@localhost:3210/randomtables?sslmode=disable"
 	db, err := sql.Open("postgres", connstr)
+	defer db.Close()
 	if err != nil {
+		log.Println("Connection to database failed")
 		log.Fatal(err)
 	}
+	if err = db.Ping(); err != nil {
+		log.Println("DB Ping Failed")
+		log.Fatal(err)
+	}
+	log.Println("DB Connection started successfully")
 
-	rows, err := db.Query("SELECT name FROM names WHERE category = Male")
+	rows, err := db.Query("SELECT name FROM names WHERE Culture = 'Celtic';")
+	if err != nil {
+		log.Println("Select query failed")
+		log.Fatal(err)
+	}
+	fmt.Printf("%t", rows.Next())
 	for rows.Next() {
 		var name string
 		if err := rows.Scan(&name); err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("%s", name)
+		fmt.Println(name)
 	}
 }
